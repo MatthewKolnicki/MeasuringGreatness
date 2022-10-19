3f20946e7d66487bdd86f7566ac6ebfba8656fe4
Matthew Kolnicki <parkourmatt@hotmail.com>
2022-10-19 10:20:58 -0400

    added libraries

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rc("font", size=14)
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
 
 if __name__ == '__main__':
     pass
