8d332cf36f639a7c3df9b0edb5fb382737fd41e9
Matthew Kolnicki <parkourmatt@hotmail.com>
2022-10-29 14:10:08 -0400

    remove unnecessary urls and imports from project

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import requests
 from sklearn import preprocessing
-import sqlite3 as sql
 import matplotlib.pyplot as plt
 import seaborn as sns
 plt.rc("font", size=14)
@@ -12,22 +11,6 @@ from sklearn.model_selection import train_test_split
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
-urls = ["https://www.basketball-reference.com/leagues/NBA_2022_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2021_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2020_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2019_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2018_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2017_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2016_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2015_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2014_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2013_advanced.html",
-        "https://www.basketball-reference.com/leagues/NBA_2012_advanced.html"
-        ]
-
 
 if __name__ == '__main__':
-    #gamedata = []
-    for url in urls:
-        x = requests.get(url)
-        print(x.content)
+    pass
