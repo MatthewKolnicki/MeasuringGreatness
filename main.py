210e31e1a00ed11684f2a1956cfc0154dd01170a
Cole Warren <cew19j@my.fsu.edu>
2022-11-29 23:22:57 -0500

    added testing.py to keep code logic implementations, created predictors and reading in values from mvp_stats

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ import requests
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 plt.rc("font", size=14)
-from bs4 import BeautifulSoup
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-sns.set(style="white")
-sns.set(style="whitegrid", color_codes=True)
+
+
+mvp_stats = pd.read_csv("player_mvp_stats.csv")
+mvp_stats = mvp_stats.fillna(0)
+del mvp_stats["Unnamed: 0"]
+mvp_stats["Player"] = mvp_stats["Player"].str.replace("*", "", regex=False)
+
+#No strings, only numbers
+#Cannot use 'Pts Won', 'Pts Max', 'Share' because share is what we are trying to predict
+# pts won and pts max are too closly cooralated with share, share = pts won / pts max
+predictors = ['Age', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P',
+       '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB',
+       'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'Year', 'W', 'L', 'W/L%', 'GB', 'PS/G',
+       'PA/G', 'SRS']
+
+training = mvp_stats[mvp_stats["Year"] < 2021]
 
 
 if __name__ == '__main__':
